3ba85757a83e3d7c1667b7b812d052e43c9f565b
z979054461 <979054461@qq.com>
2023-07-30 16:50:24 +0800

    feat: 环绕式照片墙demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { useState } from "react";
+import "./App.scss";
+
+const pictures = [
+  "./pictures/0K7A0898.jpg",
+  "./pictures/0K7A0916.jpg",
+  "./pictures/0K7A0952.jpg",
+  "./pictures/0K7A1092.jpg",
+  "./pictures/0K7A1104.jpg",
+  "./pictures/0K7A1143.jpg",
+  "./pictures/0K7A1174.jpg",
+  "./pictures/0K7A1301.jpg",
+  "./pictures/0K7A1431.jpg",
+  "./pictures/0K7A1476.jpg",
+];
+
+const pictureNumber = pictures.length;
 
 function App() {
+  const [rotateY, setRotateY] = useState(0);
+
+  // 手指的初始位置
+  const [startX, setStartX] = useState(0);
+
+  const handleClick = (index: number) => {
+    setRotateY(
+      (((pictureNumber - index + 4 + pictureNumber) % pictureNumber) * 360) /
+        pictureNumber
+    );
+  };
+
+  const handleWheel = (e: React.WheelEvent<HTMLDivElement>) => {
+    setRotateY(
+      (prev) => (prev + ((e.deltaY / 100) * 360) / pictureNumber) % 360
+    );
+  };
+
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    setStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    const currentX = e.touches[0].clientX; // 获取当前手指的位置
+    const distanceX = currentX - startX; // 计算手指移动的距离
+
+    setRotateY(
+      (prev) => (prev + ((distanceX / 100) * 360) / pictureNumber) % 360
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      onWheel={handleWheel}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      className="App"
+      style={{ transform: `rotateY(${rotateY}deg)` }}
+    >
+      {pictures.map((picture, index) => (
+        <img onClick={(e) => handleClick(index)} src={picture} alt="" />
+      ))}
     </div>
   );
 }
